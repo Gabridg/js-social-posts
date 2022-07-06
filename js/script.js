221bbf6b5817e3fae1a2f7ccbbccc82a685366aa
Gabridg <gabri99.del@gmail.com>
2022-07-06 17:21:30 +0200

    MILESTONE 2 (?)

--- js/script.js
+++ js/script.js
@@ -24,6 +24,7 @@ Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e inc
  3. Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
 # Consigli del giorno:
  Ragioniamo tanto sulla definizione dell'oggetto, se sbagliamo quello tutto diventa più difficile!
+ `
 */
 
 // MILESTONE 1
@@ -38,4 +39,45 @@ const gallery = [
         likes: '80',
     }
 ]
-console.table(gallery)
+
+console.table(gallery);
+
+// MILESTONE 2
+const postList = document.getElementById('container');
+
+const createPost = (post) => {
+    const card = `
+    <div class="post">
+    <div class="post__header">
+        <div class="post-meta">
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="" alt="${post.authorSrc}" />
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${post.author}</div>
+                <div class="post-meta__time">${post.date}</div>
+            </div>
+        </div>
+    </div>
+    <div class="post__text">${post.postText}</div>
+    <div class="post__image">
+        <img src="${post.src}" alt="" />
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+            </div>
+        </div>
+    </div>
+    </div>`
+
+    return card;
+}
+
+postList.innerHTML = createPost();
